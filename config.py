ac1f4a47236d8ce49a522a8c900f64466b9b9d09
John Thomas <jthomas@kditek.com>
2016-07-16 18:31:10 -0400

    server.sh/no_data template/config.py

--- config.py
+++ config.py
@@ -8,9 +8,6 @@ SECRET_KEY = 'c\xf5D\x909AF\xb0\xd6\x83<\x7fr\x83\xa7\xe5\x98\x96\x07\x7f\x97\x9
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
-#SQLALCHEMY_DATABASE_URI ='mssql+pymssql://jxthomas:sixers2016@192.168.168.111/?charset=utf8'
-
-SQLALCHEMY_DATABASE_URI ='mssql+pyodbc://sa:coffee2016@reportsdb_odbc'
+SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE') or 'mssql+pyodbc://sa:coffee2016@reportsdb_odbc'
+#SQLALCHEMY_DATABASE_URI ='mssql+pyodbc://sa:coffee2016@reportsdb_odbc'
 SQLALCHEMY_ECHO = True
-#SQLALCHEMY_DATABASE_URI ='mssql+pymssql://jxthomas:sixers2016@192.168.168.111/?charset=utf8'
-#SQLALCHEMY_DATABASE_URI ='mssql+pymssql://sa:coffee2016@192.168.168.111:1433/reportsdb'
---
+++ server.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+#server.sh
+
+export VERSION=1.00
+export RELEASE_DATE=8-05-2016
+
+export DATABASE=mysql://assettv:password@127.0.0.1/asset_tv
